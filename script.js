73c8f9face0057305d761d7961cadfd7d8efcd65
みそしる <sasaryo9125@gmail.com>
2024-08-10 17:57:25 +0900

    JavaScript修正

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-   //HTMLのid値を使って以下のDOM要素を取得
-const downbutton = document.getElementById('down');
-const upbutton = document.getElementById('up');
-const text = document.getElementById('textbox');
-const reset = document.getElementById('reset');
-})();
- //ボタンが押されたらカウント減
-downbutton.addEventListener('click', (event) => {
-  if(text.value >= 1) {
-    text.value--;
-  }
+  //HTMLのid値を使って以下のDOM要素を取得
+  const downbutton = document.getElementById('down');
+  const upbutton = document.getElementById('up');
+  const text = document.getElementById('textbox');
+  const reset = document.getElementById('reset');
+
+  // ボタンが押されたらカウント減
+  downbutton.addEventListener('click', () => {
+    if (text.value > 0) {
+      text.value--;
+    }
+  });
+
+  // ボタンが押されたらカウント増
+  upbutton.addEventListener('click', () => {
+    text.value++;
+  });
+
+  // リセットボタンが押されたら0に戻る
+  reset.addEventListener('click', () => {
+    text.value = 0;
+  });
 });
- //ボタンが押されたらカウント増
-upbutton.addEventListener('click', (event) => {
-  text.value++;
-})
- //ボタンが押されたら0に戻る
-reset.addEventListener('click', (event) => {
-  text.value = 0;
-})
