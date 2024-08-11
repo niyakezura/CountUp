71e7b5ff638ea5b48faaa929ae71fd2fe39c1f2b
大脇和也 <bp22057@shibaura-it.ac.jp>
2024-08-11 15:08:27 +0900

    プロパティ

--- script.js
+++ script.js
@@ -22,16 +22,16 @@ document.addEventListener('DOMContentLoaded', () => {
   // ボタンが押されたらカウント増
   upbutton.addEventListener('click', () => {
     text.value++;
-    apearnumber.value=text.value;
+    apearnumber.textContent=text.value;
     // 色違い確率ボタンが押されたら結果を代入する
-   appearresult.value= 1-(((diffcolor.value -1)/diffcolor.value)**text.value);
+   appearresult.textContent= 1-(((diffcolor.value -1)/diffcolor.value)**text.value);
   });
 
   // リセッsトボタンが押されたら0に戻る
   reset.addEventListener('click', () => {
     text.value = 0;
-    apearnumber.value=text.value;
+    apearnumber.textContent=text.value;
     // 色違い確率ボタンが押されたら結果を代入する
-   appearresult.value = 1-(((diffcolor.value -1)/diffcolor.value)**text.value);
+   appearresult.textContent = 1-(((diffcolor.value -1)/diffcolor.value)**text.value);
   });
 });
